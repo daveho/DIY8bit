6803426ac27961d4a417b4ef1318d5befe42cea7
David Hovemeyer <david.hovemeyer@gmail.com>
2021-01-12 17:27:08 -0500

    Ruby script to build keyboard translation table

---
+++ Episode13/Code/build_kbd_table.rb
@@ -0,0 +1,44 @@
+#! /usr/bin/env ruby
+
+def translate(item)
+  if item.length == 1
+    return item.ord
+  else
+    # this is a special key that doesn't have a direct encoding
+    # in ASCII, so for now, just return '?'
+    return '?'.ord
+  end
+end
+
+non_shifted = [0] * 64
+shifted = [0] * 64
+
+column = nil
+row = nil
+
+STDIN.each_line do |line|
+  line.strip!
+  if m = /^column (\d+)/.match(line)
+    column = m[1].to_i
+    row = 0
+  elsif line != ''
+    index = column*8 + row
+    items = line.split(/\s+/)
+    #puts ";; column=#{column}, row=#{row}, items=#{items.join(',')}"
+    non_shifted[index] = translate(items[0])
+    shifted[index] = translate(items.length == 2 ? items[1] : items[0])
+    row += 1
+  end
+end
+
+puts "keycode_not_shifted"
+non_shifted.each do |code|
+  puts "\tFCB #{code}"
+end
+
+puts ""
+
+puts "keycode_shifted"
+shifted.each do |code|
+  puts "\tFCB #{code}"
+end
