ba2fb8b679aabca29e120dc6af7a4b0a76791fb9
David Hovemeyer <david.hovemeyer@gmail.com>
2021-06-04 08:32:20 -0400

    successfully use external oscillator
    Dividing external 12 MHz oscillator signal by 2 yields a
    6 MHz square wave. This appears to be rock solid, and free
    from the jitter which is seen when using the internal
    high frequency oscillator as a clock.

---
+++ Episode15/Experiment03/icevga.v
@@ -0,0 +1,25 @@
+// ice40 vga device
+
+// Third test: generate a square wave using the external oscillator,
+// hopefully avoiding all of the jitter we get using the internal
+// high frequency oscillator.  The 12 MHz output pin should be
+// connected to GPIO 20.
+
+module icevga (input wire ext_osc,
+               output wire sq_out);
+
+  // Use global clock buffer to distribute the external clock signal
+  wire clk;
+  SB_GB sbGlobalBuffer_inst(.USER_SIGNAL_TO_GLOBAL_BUFFER(ext_osc),
+                            .GLOBAL_BUFFER_OUTPUT(clk));
+
+  reg [15:0] counter;
+
+  always @(posedge clk)
+    counter <= counter + 1;
+
+  // This should divide the 12 MHz oscillator frequency by 2,
+  // so we should see a 6 MHz square wave.
+  assign sq_out = counter[0];
+
+endmodule
