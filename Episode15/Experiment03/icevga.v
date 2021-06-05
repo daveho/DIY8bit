b5899f3267d3eb4419a39a9360ab0a2afa20bfd2
David Hovemeyer <david.hovemeyer@gmail.com>
2021-06-05 10:39:49 -0400

    minor formatting tweaks

--- Episode15/Experiment03/icevga.v
+++ Episode15/Experiment03/icevga.v
@@ -1,14 +1,15 @@
 // ice40 vga device
 
-// Third test: generate a square wave using the external oscillator,
-// hopefully avoiding all of the jitter we get using the internal
-// high frequency oscillator.  The 12 MHz output pin should be
-// connected to GPIO 20.
+// Third test: generate a square wave using the external
+// oscillator, hopefully avoiding all of the jitter we get
+// using the internal high frequency oscillator.  The
+// 12 MHz output pin should be connected to GPIO 20.
 
 module icevga (input wire ext_osc,
                output wire sq_out);
 
-  // Use global clock buffer to distribute the external clock signal
+  // Use global clock buffer to distribute the external
+  // clock signal
   wire clk;
   SB_GB clk_buffer(.USER_SIGNAL_TO_GLOBAL_BUFFER(ext_osc),
                    .GLOBAL_BUFFER_OUTPUT(clk));
