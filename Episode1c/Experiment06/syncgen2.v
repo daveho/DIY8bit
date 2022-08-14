bc9e9bfc1211f4835da56a2823a6fcbf5900c59e
David Hovemeyer <david.hovemeyer@gmail.com>
2022-08-14 15:55:47 -0400

    pixels are now placed correctly
    The remaining issue seems to be the character column
    and/or row not being accurate.

--- Episode1c/Experiment06/syncgen2.v
+++ Episode1c/Experiment06/syncgen2.v
@@ -3,7 +3,7 @@
 module syncgen2(input clk,                 // 40 MHz dot clock input
                 input nrst,                // active low reset input
                 output reg [15:0] hcount,  // output horizontal pixel counter
-                output reg hsync,          // output horizontal sync
+                output hsync,              // output horizontal sync
                 output reg [15:0] vcount,  // output vertical pixel counter
                 output reg vsync,          // output vertical sync
                 output reg vis             // 1 if current hcount/vcount is in visible region
@@ -11,68 +11,51 @@ module syncgen2(input clk,                 // 40 MHz dot clock input
 
   `include "timing.vh"
 
-  // We want hsync and vsync to "lag" by one clock.
-  // That way, the pixel-generation code can look at hcount and
-  // vcount_internal as an accurate representation of which pixel will
-  // be generated *next*. The hsync_next and vsync_next values
-  // are computed based on the current hcount and vcount_internal,
-  // and then copied to hsync and vsync on the next clock.
-  // Similarly, vis can be taken as indicating whether the
-  // next clock is a pixel in the visible region.
-  reg hsync_next;
-  reg vsync_next;
+  reg hsync_internal;
 
-  reg [15:0] vcount_internal;
+  // This hack seems to get horizontal placement correct on
+  // my monitor
+  delay hsync_delay(.clk(clk), .in(hsync_internal), .out(hsync));
+
+  reg [15:0] vcount_internal, vcount_delay;
 
   always @(posedge clk)
     begin
       if (nrst == 1'b0)
         begin
           // reset asserted
-
-          hsync_next <= 1'b0;
-          vsync_next <= 1'b0;
-
-          hsync <= 1'b0;
+          hsync_internal <= 1'b0;
           vsync <= 1'b0;
 
-          hcount <= 16'd2;
-
-          vcount_internal <= 16'd1;
+          hcount <= 16'd0;
           vcount <= 16'd0;
+
+          vcount_internal <= V_BACK_PORCH_END - 1;
+          vcount_delay <= V_FRONT_PORCH_END;
 
           vis <= 1'b1;
         end
 
       else
         begin
-          hsync <= hsync_next;
-          vsync <= vsync_next;
-
-/*
-          if (vcount_internal == 16'd0)
-            vcount <= V_BACK_PORCH_END;
-          else
-            vcount <= vcount_internal - 1;
-*/
-
           if (hcount == H_VISIBLE_END)
             begin
               // visible part of line ends
               hcount <= hcount + 1;
+              vis <= 1'b0;
             end
 
           else if (hcount == H_FRONT_PORCH_END)
             begin
-              // begin hsync pulse
-              hsync_next <= 1'b1;
+              // begin hsync_internal pulse
+              hsync_internal <= 1'b1;
               hcount <= hcount + 1;
             end
 
           else if (hcount == H_SYNC_PULSE_END)
             begin
-              // end hsync pulse
-              hsync_next <= 1'b0;
+              // end hsync_internal pulse
+              hsync_internal <= 1'b0;
               hcount <= hcount + 1;
             end
 
@@ -81,8 +64,10 @@ module syncgen2(input clk,                 // 40 MHz dot clock input
               // new horizontal line begins
               hcount <= 16'd0;
 
-              // vcount lags vcount_internal on update
-              vcount <= vcount_internal;
+              // I'm not sure exactly why this hack is necessary, but
+              // it seems to work
+              vcount <= vcount_delay;
+              vcount_delay <= vcount_internal;
 
               // update vcount_internal and vsync
               if (vcount_internal == V_VISIBLE_END)
@@ -93,14 +78,14 @@ module syncgen2(input clk,                 // 40 MHz dot clock input
               else if (vcount_internal == V_FRONT_PORCH_END)
                 begin
                   // begin vsync pulse
-                  vsync_next <= 1'b1;
+                  vsync <= 1'b1;
                   vcount_internal <= vcount_internal + 1;
                 end
 
               else if (vcount_internal == V_SYNC_PULSE_END)
                 begin
                   // end vsync pulse
-                  vsync_next <= 1'b0;
+                  vsync <= 1'b0;
                   vcount_internal <= vcount_internal + 1;
                 end
 
@@ -108,12 +93,14 @@ module syncgen2(input clk,                 // 40 MHz dot clock input
                 begin
                   // frame ends
                   vcount_internal <= 16'd0;
+                  vis <= 1'b1;
                 end
 
               else
                 begin
                   // move on to next line in visible region
                   vcount_internal <= vcount_internal + 1;
+                  vis <= 1'b1;
                 end
 
             end
