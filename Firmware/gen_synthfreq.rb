0e6e7f2cf5b7a31124f78a4d2a68e758fcf261e5
David Hovemeyer <david.hovemeyer@gmail.com>
2023-08-20 12:08:48 -0400

    compute rough and fine freq reg values

--- Firmware/gen_synthfreq.rb
+++ Firmware/gen_synthfreq.rb
@@ -7,6 +7,10 @@
 # Frequency of C3
 C3_FREQ = 2.0**((48 - 69) / 12.0) * 440.0
 
+# YM2149F master clock frequency is 4 MHz
+# (same as Atari ST)
+F_MASTER = 4000000.0
+
 # Pairs of fine and rough frequency values corresponding to each
 # keyboard scan code. (These are initially all set to 0, so that
 # keys not mapped to a note can be ignored.)
@@ -33,6 +37,17 @@ STDIN.each_line do |line|
   freq = note_freq(note)
 
   # TODO: compute actual YM2149F frequency register values
+  #puts "scancode=#{scancode}, column=#{column}, row=#{row}, freq=#{freq}"
 
-  puts "scancode=#{scancode}, column=#{column}, row=#{row}, freq=#{freq}"
+  # Compute TP (to nearest integer)
+  tp = (F_MASTER / (16.0 * freq)).round
+
+  # Compute rough and fine frequency register values:
+  # these are (respectively) the upper 4 bits and lower 8
+  # bits of TP considered as a 12-bit unsigned integer
+  rough = (tp >> 8) & 0xF
+  fine = tp & 0xFF
+
+  regvals[scancode] = [rough, fine]
+  puts "scancode=#{scancode}, column=#{column}, row=#{row}, freq=#{freq}, rough=#{rough}, fine=#{fine}"
 end
