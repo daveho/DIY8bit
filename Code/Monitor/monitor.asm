42a2c740fd9af91b9ef22f86b03d4eb2900dcead
David Hovemeyer <david.hovemeyer@gmail.com>
2024-07-18 19:46:29 -0400

    more work on hwvga routines, untested

--- Code/Monitor/monitor.asm
+++ Code/Monitor/monitor.asm
@@ -1215,6 +1215,9 @@ hwvga_init
 
 	rts
 
+;; Default handler for vertical refresh interrupt (IRQ6).
+;; Handles blinking the cursor (which, like the C64, is done
+;; in software.)
 hwvga_irq_handler
 	lda hwvga_irq_count           ; how many vrefresh irqs have occurred?
 	cmpa #30                      ; less than 30?
@@ -1226,11 +1229,10 @@ hwvga_irq_handler
 	jsr hwvga_compute_addr        ; compute VRAM address
 	jsr hwvga_map_bank            ; access address within bank (and switch to that bank)
 	lda 1,X                       ; get attribute
-	rora                          ; swap fg and bg
-	rora
-	rora
-	rora
-	sta 1,X                       ; set inverted fg/bg at cursor position
+	; swap fg/bg of attribute
+	ldy #REVERSE_ATTR             ; load address of attribute reversal table
+	lda A,Y                       ; get reversed attribute
+	sta 1,X                       ; set reversed fg/bg at cursor position
 	lda #0
 	sta hwvga_irq_count           ; set counter back to 0
 	jmp 99f                       ; done
@@ -1243,23 +1245,49 @@ hwvga_irq_handler
 	jsr reset_irq6_ff
 	rts
 
-;; 
+;; Compute the VRAM address of the first byte of the character cell
+;; at specified row and column.
+;; Parameters:
+;;   A - the row (should be in range 0-29)
+;;   B - the column (should be in range 0-79)
+;; Return (in X register):
+;;   the VRAM address of the first byte of the character cell
+;; Clobbers Y
 hwvga_compute_addr
-	; Pseudo code
-	; X: computed address
-	; Y: temp value
-	; note: x*160 = (x*2 + x*8) * 16
-	; set X to 0
-	; move A (row) to Y
-	; shift Y left by 2 (times 2)
-	; add Y to X
-	; shift Y left by 6 (times 8)
-	; add Y to X
-	; shift X by 4 (times 16)
-
+	pshs B                        ; save column
+	ldb #0
+	pshs B                        ; push 0 byte onto stack
+	tfr S,Y                       ; Y points to 0-extended column value
+	ldb #160                      ; number of bytes per row
+	mul                           ; set D to row*160
+	addd ,Y                       ; add column to row*160
+	puls X                        ; clear 0-extended column from stack
+	tfr D,X                       ; move computed address to X
 	rts
 
+;; Map the VRAM address in X to a bank.
+;; Selects the appropriate bank, and adjusts the value in X
+;; so that it is the correct absolute address in the VRAM
+;; window to refer to the original VRAM address.
+;; Clobbers A and B.
 hwvga_map_bank
+	tfr X,D                       ; move VRAM address to D
+	ldx #0                        ; X is bank (initially assume bank 0)
+
+1
+	cmpd #2048                    ; check whether we're reached the correct bank
+	ble 99f                       ; if so, exit loop
+	leax 1,X                      ; add 1 to X
+	subd #2048                    ; move past one bank
+	jmp 1b
+
+99
+	addd #HWVGA_VRAM              ; add VRAM window base address to adjusted VRAM addr
+	pshs D                        ; save computed VRAM addr
+	tfr X,D                       ; move bank to A:B (bank will be in B)
+	orb hwvga_cur_font            ; keep font bits the same
+	stb HWVGA_BANKREG             ; set bank
+	puls X                        ; pop computed VRAM addr into X
 
 	rts
 
@@ -1391,6 +1419,14 @@ CAT_PICTURE
 ;;**********************************************************************
 
 	INCLUDE "tms9918a_font.asm"
+
+;;**********************************************************************
+;; Table to look up reversals of text attributes.
+;; Arguably wastes 256 bytes of ROM, but I'm lazy.
+;;**********************************************************************
+
+REVERSE_ATTR
+	INCLUDE "revattr.asm"
 
 ;;**********************************************************************
 ;; System address table
