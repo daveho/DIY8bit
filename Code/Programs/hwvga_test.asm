13392827eb8f82da311682a7abb7a9fc0bc8d3ef
David Hovemeyer <david.hovemeyer@gmail.com>
2024-07-19 16:24:32 -0400

    test modifying char/attr in a higher bank
    Seems to work fine.

--- Code/Programs/hwvga_test.asm
+++ Code/Programs/hwvga_test.asm
@@ -16,11 +16,21 @@ entry
 	; At this point, X should contain the correct address to write
 	; to in the VRAM window in order to modify the character cell
 	; at row 3, column 5
-
-	rts
-
 	lda #1                        ; smiley face
 	sta ,X
+
+
+	lda #24
+	ldb #79
+	ldy OFFTAB_HWVGA_COMPUTE_ADDR
+	jsr ,y
+	ldy OFFTAB_HWVGA_MAP_BANK
+	jsr ,y
+	lda #234                       ; omega
+	sta ,x
+	lda #(HWVGA_BG_MAGENTA|HWVGA_FG_BR_GREEN)
+	sta 1,x
+
 	rts
 
 ;; vim:ft=asm6809:
