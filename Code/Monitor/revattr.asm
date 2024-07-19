58cb9e07a97c3d10605eda4120ac573eaae0d7ac
David Hovemeyer <david.hovemeyer@gmail.com>
2024-07-18 20:13:33 -0400

    minor improvements

--- Code/Monitor/revattr.asm
+++ Code/Monitor/revattr.asm
@@ -1,256 +1,256 @@
-	FCB $0
-	FCB $10
-	FCB $20
-	FCB $30
-	FCB $40
-	FCB $50
-	FCB $60
-	FCB $70
-	FCB $80
-	FCB $90
-	FCB $a0
-	FCB $b0
-	FCB $c0
-	FCB $d0
-	FCB $e0
-	FCB $f0
-	FCB $1
-	FCB $11
-	FCB $21
-	FCB $31
-	FCB $41
-	FCB $51
-	FCB $61
-	FCB $71
-	FCB $81
-	FCB $91
-	FCB $a1
-	FCB $b1
-	FCB $c1
-	FCB $d1
-	FCB $e1
-	FCB $f1
-	FCB $2
-	FCB $12
-	FCB $22
-	FCB $32
-	FCB $42
-	FCB $52
-	FCB $62
-	FCB $72
-	FCB $82
-	FCB $92
-	FCB $a2
-	FCB $b2
-	FCB $c2
-	FCB $d2
-	FCB $e2
-	FCB $f2
-	FCB $3
-	FCB $13
-	FCB $23
-	FCB $33
-	FCB $43
-	FCB $53
-	FCB $63
-	FCB $73
-	FCB $83
-	FCB $93
-	FCB $a3
-	FCB $b3
-	FCB $c3
-	FCB $d3
-	FCB $e3
-	FCB $f3
-	FCB $4
-	FCB $14
-	FCB $24
-	FCB $34
-	FCB $44
-	FCB $54
-	FCB $64
-	FCB $74
-	FCB $84
-	FCB $94
-	FCB $a4
-	FCB $b4
-	FCB $c4
-	FCB $d4
-	FCB $e4
-	FCB $f4
-	FCB $5
-	FCB $15
-	FCB $25
-	FCB $35
-	FCB $45
-	FCB $55
-	FCB $65
-	FCB $75
-	FCB $85
-	FCB $95
-	FCB $a5
-	FCB $b5
-	FCB $c5
-	FCB $d5
-	FCB $e5
-	FCB $f5
-	FCB $6
-	FCB $16
-	FCB $26
-	FCB $36
-	FCB $46
-	FCB $56
-	FCB $66
-	FCB $76
-	FCB $86
-	FCB $96
-	FCB $a6
-	FCB $b6
-	FCB $c6
-	FCB $d6
-	FCB $e6
-	FCB $f6
-	FCB $7
-	FCB $17
-	FCB $27
-	FCB $37
-	FCB $47
-	FCB $57
-	FCB $67
-	FCB $77
-	FCB $87
-	FCB $97
-	FCB $a7
-	FCB $b7
-	FCB $c7
-	FCB $d7
-	FCB $e7
-	FCB $f7
-	FCB $8
-	FCB $18
-	FCB $28
-	FCB $38
-	FCB $48
-	FCB $58
-	FCB $68
-	FCB $78
-	FCB $88
-	FCB $98
-	FCB $a8
-	FCB $b8
-	FCB $c8
-	FCB $d8
-	FCB $e8
-	FCB $f8
-	FCB $9
-	FCB $19
-	FCB $29
-	FCB $39
-	FCB $49
-	FCB $59
-	FCB $69
-	FCB $79
-	FCB $89
-	FCB $99
-	FCB $a9
-	FCB $b9
-	FCB $c9
-	FCB $d9
-	FCB $e9
-	FCB $f9
-	FCB $a
-	FCB $1a
-	FCB $2a
-	FCB $3a
-	FCB $4a
-	FCB $5a
-	FCB $6a
-	FCB $7a
-	FCB $8a
-	FCB $9a
-	FCB $aa
-	FCB $ba
-	FCB $ca
-	FCB $da
-	FCB $ea
-	FCB $fa
-	FCB $b
-	FCB $1b
-	FCB $2b
-	FCB $3b
-	FCB $4b
-	FCB $5b
-	FCB $6b
-	FCB $7b
-	FCB $8b
-	FCB $9b
-	FCB $ab
-	FCB $bb
-	FCB $cb
-	FCB $db
-	FCB $eb
-	FCB $fb
-	FCB $c
-	FCB $1c
-	FCB $2c
-	FCB $3c
-	FCB $4c
-	FCB $5c
-	FCB $6c
-	FCB $7c
-	FCB $8c
-	FCB $9c
-	FCB $ac
-	FCB $bc
-	FCB $cc
-	FCB $dc
-	FCB $ec
-	FCB $fc
-	FCB $d
-	FCB $1d
-	FCB $2d
-	FCB $3d
-	FCB $4d
-	FCB $5d
-	FCB $6d
-	FCB $7d
-	FCB $8d
-	FCB $9d
-	FCB $ad
-	FCB $bd
-	FCB $cd
-	FCB $dd
-	FCB $ed
-	FCB $fd
-	FCB $e
-	FCB $1e
-	FCB $2e
-	FCB $3e
-	FCB $4e
-	FCB $5e
-	FCB $6e
-	FCB $7e
-	FCB $8e
-	FCB $9e
-	FCB $ae
-	FCB $be
-	FCB $ce
-	FCB $de
-	FCB $ee
-	FCB $fe
-	FCB $f
-	FCB $1f
-	FCB $2f
-	FCB $3f
-	FCB $4f
-	FCB $5f
-	FCB $6f
-	FCB $7f
-	FCB $8f
-	FCB $9f
-	FCB $af
-	FCB $bf
-	FCB $cf
-	FCB $df
-	FCB $ef
-	FCB $ff
+	FCB $00 ; (00)
+	FCB $10 ; (01)
+	FCB $20 ; (02)
+	FCB $30 ; (03)
+	FCB $40 ; (04)
+	FCB $50 ; (05)
+	FCB $60 ; (06)
+	FCB $70 ; (07)
+	FCB $80 ; (08)
+	FCB $90 ; (09)
+	FCB $a0 ; (0a)
+	FCB $b0 ; (0b)
+	FCB $c0 ; (0c)
+	FCB $d0 ; (0d)
+	FCB $e0 ; (0e)
+	FCB $f0 ; (0f)
+	FCB $01 ; (10)
+	FCB $11 ; (11)
+	FCB $21 ; (12)
+	FCB $31 ; (13)
+	FCB $41 ; (14)
+	FCB $51 ; (15)
+	FCB $61 ; (16)
+	FCB $71 ; (17)
+	FCB $81 ; (18)
+	FCB $91 ; (19)
+	FCB $a1 ; (1a)
+	FCB $b1 ; (1b)
+	FCB $c1 ; (1c)
+	FCB $d1 ; (1d)
+	FCB $e1 ; (1e)
+	FCB $f1 ; (1f)
+	FCB $02 ; (20)
+	FCB $12 ; (21)
+	FCB $22 ; (22)
+	FCB $32 ; (23)
+	FCB $42 ; (24)
+	FCB $52 ; (25)
+	FCB $62 ; (26)
+	FCB $72 ; (27)
+	FCB $82 ; (28)
+	FCB $92 ; (29)
+	FCB $a2 ; (2a)
+	FCB $b2 ; (2b)
+	FCB $c2 ; (2c)
+	FCB $d2 ; (2d)
+	FCB $e2 ; (2e)
+	FCB $f2 ; (2f)
+	FCB $03 ; (30)
+	FCB $13 ; (31)
+	FCB $23 ; (32)
+	FCB $33 ; (33)
+	FCB $43 ; (34)
+	FCB $53 ; (35)
+	FCB $63 ; (36)
+	FCB $73 ; (37)
+	FCB $83 ; (38)
+	FCB $93 ; (39)
+	FCB $a3 ; (3a)
+	FCB $b3 ; (3b)
+	FCB $c3 ; (3c)
+	FCB $d3 ; (3d)
+	FCB $e3 ; (3e)
+	FCB $f3 ; (3f)
+	FCB $04 ; (40)
+	FCB $14 ; (41)
+	FCB $24 ; (42)
+	FCB $34 ; (43)
+	FCB $44 ; (44)
+	FCB $54 ; (45)
+	FCB $64 ; (46)
+	FCB $74 ; (47)
+	FCB $84 ; (48)
+	FCB $94 ; (49)
+	FCB $a4 ; (4a)
+	FCB $b4 ; (4b)
+	FCB $c4 ; (4c)
+	FCB $d4 ; (4d)
+	FCB $e4 ; (4e)
+	FCB $f4 ; (4f)
+	FCB $05 ; (50)
+	FCB $15 ; (51)
+	FCB $25 ; (52)
+	FCB $35 ; (53)
+	FCB $45 ; (54)
+	FCB $55 ; (55)
+	FCB $65 ; (56)
+	FCB $75 ; (57)
+	FCB $85 ; (58)
+	FCB $95 ; (59)
+	FCB $a5 ; (5a)
+	FCB $b5 ; (5b)
+	FCB $c5 ; (5c)
+	FCB $d5 ; (5d)
+	FCB $e5 ; (5e)
+	FCB $f5 ; (5f)
+	FCB $06 ; (60)
+	FCB $16 ; (61)
+	FCB $26 ; (62)
+	FCB $36 ; (63)
+	FCB $46 ; (64)
+	FCB $56 ; (65)
+	FCB $66 ; (66)
+	FCB $76 ; (67)
+	FCB $86 ; (68)
+	FCB $96 ; (69)
+	FCB $a6 ; (6a)
+	FCB $b6 ; (6b)
+	FCB $c6 ; (6c)
+	FCB $d6 ; (6d)
+	FCB $e6 ; (6e)
+	FCB $f6 ; (6f)
+	FCB $07 ; (70)
+	FCB $17 ; (71)
+	FCB $27 ; (72)
+	FCB $37 ; (73)
+	FCB $47 ; (74)
+	FCB $57 ; (75)
+	FCB $67 ; (76)
+	FCB $77 ; (77)
+	FCB $87 ; (78)
+	FCB $97 ; (79)
+	FCB $a7 ; (7a)
+	FCB $b7 ; (7b)
+	FCB $c7 ; (7c)
+	FCB $d7 ; (7d)
+	FCB $e7 ; (7e)
+	FCB $f7 ; (7f)
+	FCB $08 ; (80)
+	FCB $18 ; (81)
+	FCB $28 ; (82)
+	FCB $38 ; (83)
+	FCB $48 ; (84)
+	FCB $58 ; (85)
+	FCB $68 ; (86)
+	FCB $78 ; (87)
+	FCB $88 ; (88)
+	FCB $98 ; (89)
+	FCB $a8 ; (8a)
+	FCB $b8 ; (8b)
+	FCB $c8 ; (8c)
+	FCB $d8 ; (8d)
+	FCB $e8 ; (8e)
+	FCB $f8 ; (8f)
+	FCB $09 ; (90)
+	FCB $19 ; (91)
+	FCB $29 ; (92)
+	FCB $39 ; (93)
+	FCB $49 ; (94)
+	FCB $59 ; (95)
+	FCB $69 ; (96)
+	FCB $79 ; (97)
+	FCB $89 ; (98)
+	FCB $99 ; (99)
+	FCB $a9 ; (9a)
+	FCB $b9 ; (9b)
+	FCB $c9 ; (9c)
+	FCB $d9 ; (9d)
+	FCB $e9 ; (9e)
+	FCB $f9 ; (9f)
+	FCB $0a ; (a0)
+	FCB $1a ; (a1)
+	FCB $2a ; (a2)
+	FCB $3a ; (a3)
+	FCB $4a ; (a4)
+	FCB $5a ; (a5)
+	FCB $6a ; (a6)
+	FCB $7a ; (a7)
+	FCB $8a ; (a8)
+	FCB $9a ; (a9)
+	FCB $aa ; (aa)
+	FCB $ba ; (ab)
+	FCB $ca ; (ac)
+	FCB $da ; (ad)
+	FCB $ea ; (ae)
+	FCB $fa ; (af)
+	FCB $0b ; (b0)
+	FCB $1b ; (b1)
+	FCB $2b ; (b2)
+	FCB $3b ; (b3)
+	FCB $4b ; (b4)
+	FCB $5b ; (b5)
+	FCB $6b ; (b6)
+	FCB $7b ; (b7)
+	FCB $8b ; (b8)
+	FCB $9b ; (b9)
+	FCB $ab ; (ba)
+	FCB $bb ; (bb)
+	FCB $cb ; (bc)
+	FCB $db ; (bd)
+	FCB $eb ; (be)
+	FCB $fb ; (bf)
+	FCB $0c ; (c0)
+	FCB $1c ; (c1)
+	FCB $2c ; (c2)
+	FCB $3c ; (c3)
+	FCB $4c ; (c4)
+	FCB $5c ; (c5)
+	FCB $6c ; (c6)
+	FCB $7c ; (c7)
+	FCB $8c ; (c8)
+	FCB $9c ; (c9)
+	FCB $ac ; (ca)
+	FCB $bc ; (cb)
+	FCB $cc ; (cc)
+	FCB $dc ; (cd)
+	FCB $ec ; (ce)
+	FCB $fc ; (cf)
+	FCB $0d ; (d0)
+	FCB $1d ; (d1)
+	FCB $2d ; (d2)
+	FCB $3d ; (d3)
+	FCB $4d ; (d4)
+	FCB $5d ; (d5)
+	FCB $6d ; (d6)
+	FCB $7d ; (d7)
+	FCB $8d ; (d8)
+	FCB $9d ; (d9)
+	FCB $ad ; (da)
+	FCB $bd ; (db)
+	FCB $cd ; (dc)
+	FCB $dd ; (dd)
+	FCB $ed ; (de)
+	FCB $fd ; (df)
+	FCB $0e ; (e0)
+	FCB $1e ; (e1)
+	FCB $2e ; (e2)
+	FCB $3e ; (e3)
+	FCB $4e ; (e4)
+	FCB $5e ; (e5)
+	FCB $6e ; (e6)
+	FCB $7e ; (e7)
+	FCB $8e ; (e8)
+	FCB $9e ; (e9)
+	FCB $ae ; (ea)
+	FCB $be ; (eb)
+	FCB $ce ; (ec)
+	FCB $de ; (ed)
+	FCB $ee ; (ee)
+	FCB $fe ; (ef)
+	FCB $0f ; (f0)
+	FCB $1f ; (f1)
+	FCB $2f ; (f2)
+	FCB $3f ; (f3)
+	FCB $4f ; (f4)
+	FCB $5f ; (f5)
+	FCB $6f ; (f6)
+	FCB $7f ; (f7)
+	FCB $8f ; (f8)
+	FCB $9f ; (f9)
+	FCB $af ; (fa)
+	FCB $bf ; (fb)
+	FCB $cf ; (fc)
+	FCB $df ; (fd)
+	FCB $ef ; (fe)
+	FCB $ff ; (ff)
